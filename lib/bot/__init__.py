83232e5443e5cb9addff13f5736c7dcaff17b009
cotidie <daily142857@gmail.com>
2021-08-20 23:43:31 +0900

    Make a working Bot

---
+++ lib/bot/__init__.py
@@ -0,0 +1,46 @@
+from discord.ext.commands import Bot as BotBase
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+# 봇이 반응할 접두사
+PREFIX = "!"
+OWNER_IDS = [525193576716566528]
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX     = PREFIX    # 봇이 반응할 접두사
+        self.guild      = None      # 속할 서버
+        self.ready      = False
+        self.scheduler  = AsyncIOScheduler()    # 코드의 주기적 수행
+
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+
+    def run(self, version:str):
+        self.VERSION = version
+
+        # 토큰 획득
+        # Q. 토큰의 역할?
+        with open("./lib/bot/token.0", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("아늑 봇이 정신을 차리고 있습니다...")
+        super().run(self.TOKEN, reconnect=True)
+
+    async def on_connect(self):
+        print("아늑이가 깨어났습니다!")
+
+    async def on_disconnect(self):
+        print("아늑이가 떠났습니다...")
+
+    async def on_ready(self):
+        if not self.ready:
+            print("아늑이가 준비를 마쳤습니다!")
+            self.guild = self.get_guild(878239436381495336)
+            self.ready=True
+        else:
+            print("아늑이가 아직 정신을 못차렸습니다.")
+    
+    async def on_message(self, message:str):
+        pass
+
+# 봇 인스턴스 생성
+bot = Bot()
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import asyncio
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+async def main():
+    print(1)
+    task = asyncio.create_task(test())
+    print("HELLO")
+    await task
 
+async def test():
+    await asyncio.sleep(1)
+    print("WOW")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+async def foo():
+    print("FOO")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+asyncio.run(main())
