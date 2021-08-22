1c1e0e65b5ca79912f0c08fe117f1ddb3d2b165f
cotidie <daily142857@gmail.com>
2021-08-23 04:03:08 +0900

    Create helpers to assist string formatting

---
+++ lib/helpers/format.py
@@ -0,0 +1,32 @@
+import datetime as dt
+
+
+class Formatter:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_date_string(date):
+        """
+        datetime.datetime 또는 datetime.date를 문자열로 변환한다.
+        :param date: (datetime.datetime) || (datetime.date)
+        :return: (str) 2021-08-22 형식의 문자열
+        """
+        # datetime이 아니면 date 객체
+        if not isinstance(date, dt.datetime):
+            return str(date)
+        else:
+            return str(date.date())
+
+    @staticmethod
+    def missions_to_list(missions:list):
+        """
+        오늘의미션 쿼리 탐색 결과를 문자열로 가공한다.
+        :param missions: MongoDB find로부터 얻은 결과
+        :return: (list) 오늘의 미션 정보
+        """
+        result = []
+        for mission in missions:
+            result.append(mission['event'])
+
+        return result
