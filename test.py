3077cf68292905e9d3854b230b055b7fbf679f0c
cotidie <daily142857@gmail.com>
2021-08-22 16:43:25 +0900

    Add a comment for debug mode

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import sys
+from os.path import isfile
+
 from glob import glob
 import requests
 from requests_html import HTMLSession
@@ -9,10 +12,22 @@ def test_dynamic_render():
     s = HTMLSession()
     r = s.get(url)
     r.html.render()
-    print(r.html.html)
+    print(type(r.html.html))
 
-request = requests.get('https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find')
-html = request.text
+def file_read():
+    if not isfile("db_host.0"):
+        print("db_host.0을 찾지 못했습니다.")
+    else:
+        with open("db_host.0") as file:
+            print(file.read())
 
-bs = BeautifulSoup(html, 'html.parser')
-print(type(bs.find("div")))
+def check_debug_mode():
+    gettrace = getattr(sys, 'gettraace', lambda: None)
+
+    # sys에 gettrace가 없다면 실행 모드
+    if gettrace is None:
+        print("We are running on Run mode")
+    else:
+        print("We are on Debug Mode")
+
+test_dynamic_render()
