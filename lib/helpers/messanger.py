dec71e8c79523e9252204553e9cefcc5551dd1e0
cotidie <daily142857@gmail.com>
2021-08-24 21:35:59 +0900

    Change some texts

--- lib/helpers/messanger.py
+++ lib/helpers/messanger.py
@@ -1,11 +1,12 @@
 from discord import Embed
-from config  import CONFIG
-from datetime   import date
+from config import CONFIG
+from datetime import date
 
 """
     봇의 디스코드 채팅을 위한 메시지 빌더
     정해진 형식의 Embed, 문자열 등을 정의합니다.
 """
+
 
 class Messenger:
     def __init__(self, bot):
@@ -20,8 +21,8 @@ class Messenger:
         """
         # 상단 설명란
         desc = "에린 세계의 여러 잡일을 도맡아 해드리고 있어요.\n" + \
-                "제가 할 수 있는 일은 '!도와줘'를 통해 알 수 있어요. \n\n" + \
-                "[여기](https://github.com/Cotidie/DiscordBot)에서는 제 해부도(?)를 볼 수 있습니다.\n"
+               "제가 할 수 있는 일은 '!도와줘'를 통해 알 수 있어요. \n\n" + \
+               "제 해부도(?)를 보시려면 [여기](https://github.com/Cotidie/DiscordBot)를 클릭하세요.\n"
         embed = Embed(title="아늑이!", description=desc,
                       color=self.color)
 
@@ -40,10 +41,9 @@ class Messenger:
         embed.set_thumbnail(url=self.image)
 
         # 꼬리말
-        embed.set_footer(text="아늑이 - BETA")
+        embed.set_footer(text="아늑이 - ALPHA")
 
         return embed
 
     def change_status(self, state):
         pass
-
