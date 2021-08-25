3c37c4ee4a676400205d7829504cd3a2eb5f7663
cotidie <daily142857@gmail.com>
2021-08-26 01:22:11 +0900

    Add show_raid_info command

--- lib/cogs/scrape.py
+++ lib/cogs/scrape.py
@@ -3,12 +3,40 @@ from datetime import date
 
 # discord.py
 from discord.ext.commands.cog import Cog
-from discord.ext.commands     import command
-from discord_slash            import cog_ext, SlashContext
+from discord.ext.commands import command
+from discord_slash import cog_ext, SlashContext
+from discord_slash.utils.manage_commands import create_option, create_choice
 
 # 커스텀 객체
 from lib.scrapers.sigkill import sigkill_scraper
-from lib.bot              import GUILD
+from lib.bot import GUILD
+from lib.db import DB
+
+# 관련 메소드가 많아지면 helper > option.py로 분리
+def create_raid_option():
+    """
+    레이드 정보 선택을 위한 옵션을 반환
+    :return: create_option()의 반환값
+    """
+    choices = [create_choice(
+        name="현재",
+        value="현재"
+    )]
+    for boss in DB.get_raid_bosses():
+        choices.append(create_choice(
+            name=boss,
+            value=boss
+        ))
+
+    option = create_option(
+        name="boss",
+        description="어떤 레이드 보스를 알려드릴까요?",
+        required=True,
+        option_type=3,
+        choices=choices
+    )
+
+    return option
 
 
 class ScrapeCog(Cog):
@@ -37,6 +65,13 @@ class ScrapeCog(Cog):
             result += event + "\n"
 
         await ctx.send(result)
+
+    # TODO: 옵션 설정 부분 Formatter로 분리
+    @cog_ext.cog_slash(name="레이드",
+                       guild_ids=[GUILD],
+                       options=[create_raid_option()])
+    async def show_raid_info(self, ctx: SlashContext, boss: str):
+        print(boss)
 
     def show_official_notice(self):
         pass
