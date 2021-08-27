c85eb808b1c89d533eeac3204e0d4ad1dff95ddf
cotidie <daily142857@gmail.com>
2021-08-28 03:20:34 +0900

    Edit __init__.py for better import

--- lib/db/collections/__init__.py
+++ lib/db/collections/__init__.py
@@ -1,5 +1,6 @@
 from .raid_time import RaidTime
 from .event_today import EventToday
 from .raid_info import RaidInfo
+from .config import Config, Keys as ConfKeys
 
-__all__ = ['EventToday', 'RaidTime', 'RaidInfo']
+__all__ = ['EventToday', 'RaidTime', 'RaidInfo', 'Config', 'ConfKeys']
