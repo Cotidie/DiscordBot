5f13f3a209e60218e25c397e9d86532a88da185f
cotidie <daily142857@gmail.com>
2021-09-03 03:57:30 +0900

    Change logic for scaping today's missions

--- lib/scrapers/sigkill.py
+++ lib/scrapers/sigkill.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from datetime   import datetime
+from datetime   import date
 
 from config                 import CONFIG
 from lib.scrapers.scraper   import Scraper
@@ -13,31 +13,41 @@ class SigKillScraper(Scraper):
         super().__init__()
 
     # 먼저 DB에 존재하는지 확인한다.
-    def get_today_missions(self, today):
+    def get_today_missions(self, today: date):
         # DB에 이미 존재하는지 확인한다.
         missions = DB.get_today_missions(today)
         if not missions:
             # 스크레이핑 실행
-            missions = self.scrape_today_missions()
+            missions = self.scrape_today_missions(today)
+            if not missions:
+                return
+
             # DB에 저장
             for mission in missions:
                 DB.insert_today_missions(today, mission)
 
         return missions
 
-    def scrape_today_missions(self):
+    def scrape_today_missions(self, today: date):
         html = self.get_html(URL_SIGKILL, dynamic=True)
         parser = BeautifulSoup(html, 'html.parser')
 
-        # 1. class=fc-today, tag=td를 찾는다.
-        today_td = parser.find("td", class_="fc-today")
+        # 1. 해당 날짜 셀을 찾는다.
+        divs = parser.find_all("div", class_="fc-content-skeleton")
+        today_td = None
+        for div in divs:
+            today_td = div.find("td", {'data-date': str(today)})
+            if today_td:
+                break
+
+        if not today_td:
+            return None
 
         # 2. tr에서 몇 번째 자식인지 구한다.
         index = self.get_index_from_parent(today_td)
 
-        # 3. fc-row 클래스를 가진 div까지 올라간다.
-        p_div = today_td.find_parent("div", class_="fc-row")
-        table = p_div.find("div", class_="fc-content-skeleton")
+        # 3. table까지 올라간다.
+        table = today_td.find_parent("table")
 
         # 4. tbody 자식의 모든 tr을 가져온다.
         trs = table.find("tbody").find_all("tr")
