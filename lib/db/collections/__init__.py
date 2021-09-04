814d7b993118a247663411bfc398940a962f7dbd
cotidie <daily142857@gmail.com>
2021-09-04 23:53:11 +0900

    Create a enum class for db fields

--- lib/db/collections/__init__.py
+++ lib/db/collections/__init__.py
@@ -1,6 +1,6 @@
 from .raid_time import RaidTime
 from .event_today import EventToday
-from .raid_info import RaidInfo
+from .raid_info import RaidInfo, Keys as InfoKeys
 from .config import Config, Keys as ConfKeys
 
-__all__ = ['EventToday', 'RaidTime', 'RaidInfo', 'Config', 'ConfKeys']
+__all__ = ['EventToday', 'RaidTime', 'RaidInfo', 'Config', 'ConfKeys', 'InfoKeys']
