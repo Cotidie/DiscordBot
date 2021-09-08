df4856db3f1b96fa0f3215e120bf0a6376f6c2fe
WonSeok <daily142857@gmail.com>
2021-09-09 01:30:47 +0900

    Add command for showing daily effects

--- lib/cogs/guide.py
+++ lib/cogs/guide.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 
 from lib.bot            import GUILDS
+from lib.db             import DB
 from lib.helpers        import Formatter
 from lib.helpers.option import OptionMaker
 """
@@ -15,20 +16,23 @@ class GuideCog(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="오늘의혜택",
+    @cog_ext.cog_slash(name="오늘의효과",
                        guild_ids=GUILDS,
                        options=OptionMaker.today_effect_options())
-    async def show_today_effect(self, ctx: SlashContext, which: str, day: int):
+    async def show_today_effect(self, ctx: SlashContext, day: int):
         """
-            요일별 혜택을 알려드릴게요!
+            요일별 효과를 알려드릴게요!
         """
         if day == -1:
-            day = Formatter.get_korean_time('date').weekday()
+            today = Formatter.get_korean_time('datetime')
+            day = today.weekday()
+            # 오전 7시 이전이면 1을 뺀다
+            if today.hour < 7:
+                day = 6 if day == 0 else day-1
 
-        if which == '어드밴스드':
-            await ctx.send("이 기능은 아직 준비중이에요 ;_;")
-        elif which == '요일':
-            await ctx.send("이 기능은 아직 준비중이에요 ;_;")
+        info = DB.get_day_effect(day)
+        embed = self.bot.messenger.embed_day_effect(info)
+        await ctx.send(embed=embed)
 
     @commands.command(name="초보자가이드")
     async def show_guide(self, ctx):
